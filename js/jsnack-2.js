ad671643676a4726c4ef43ce240f03519909d6a1
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-09 11:29:25 +0200

    j snack 3

--- js/jsnack-2.js
+++ js/jsnack-2.js
@@ -8,7 +8,21 @@ console.log(parola_2.length);
 if (parola_1.length > parola_2.length) {
   console.log(parola_1);
   console.log(parola_2);
-} else {
+} else if (parola_1.length < parola_2.length) {
   console.log(parola_2);
   console.log(parola_1);
+} else {
+  alert("le parole hanno stessa lunghezza.");
 }
+
+//Esercizio 3
+
+var somma = 0;
+var number;
+for (var i = 1; i <= 10; i++) {
+  number = parseInt(prompt("Inserisci un numero: "));
+  somma += number;
+  console.log(somma);
+}
+
+console.log("Ecco la somma: ", somma);
---
+++ js/jsnack-3.js
@@ -0,0 +1,11 @@
+//Esercizio 3
+
+var somma = 0;
+var number;
+for (var i = 1; i <= 10; i++) {
+  number = parseInt(prompt("Inserisci un numero: "));
+  somma += number;
+  console.log(somma);
+}
+
+console.log("Ecco la somma: ", somma);
