519bba2d2db4475f486ac27d058e34d17224b1ef
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-09 10:46:33 +0200

    jsnack 2 completato

---
+++ js/jsnack-2.js
@@ -0,0 +1,11 @@
+var parola_1 = prompt("Inserisci la prima parola: ");
+
+var parola_2 = prompt("Inserisci la seconda parola: ");
+
+if (parola_1.length > parola_2.length) {
+  console.log(parola_1);
+  console.log(parola_2);
+} else {
+  console.log(parola_2);
+  console.log(parola_1);
+}
