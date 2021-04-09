01512014967adf8125bb20962ffa54facc3ee16e
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-09 12:17:32 +0200

    Esercizio 4 aggiunte html

--- js/jsnack-4.js
+++ js/jsnack-4.js
@@ -1,10 +1,12 @@
 //Esercizio 4
-var ilTuoNome = "Enrico";
+var ilTuoNome = prompt("Buonasera. Il suo nome?");
 var invitati = ["Luca", "Filippo", "Alessandro", "Marco", "Francesco"];
 var inLista = false;
+var risultato = "";
 
 for (var i = 0; i < invitati.length; i++) {
   var nomeLista = invitati[i];
+  console.log(nomeLista);
   if (ilTuoNome === nomeLista) {
     inLista = true;
     break;
@@ -13,6 +15,11 @@ for (var i = 0; i < invitati.length; i++) {
 
 if (inLista) {
   console.log("Nominativo trovato. Benvenuto!");
+  risultato = "Nominativo trovato. Benvenuto!";
 } else {
   console.log("Siamo spiacenti. Il suo nome non è in lista.");
+  risultato = "Siamo spiacenti. Il suo nome non è in lista.";
 }
+
+document.getElementById("nome").innerHTML = "Il tuo nome: " + ilTuoNome;
+document.getElementById("risultato").innerHTML = risultato;
