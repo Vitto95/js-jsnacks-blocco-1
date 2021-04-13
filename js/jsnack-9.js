aae9b31483e605440906259f319595d613dcfc60
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-13 16:50:26 +0200

    Esercizio 9 senza array completato

--- js/jsnack-9.js
+++ js/jsnack-9.js
@@ -2,13 +2,13 @@
 
 var sum = 0;
 
-//calcolo la media dei primi 10 numeri interi
-
 for (var i = 1; i <= 10; i++) {
   var number = parseInt(prompt(`Inserisci il ${i}° numero: `));
   if (!isNaN(number)) {
     console.log(`${i}° numero:` + number);
     sum += number;
+    document.getElementById("number-avg").innerHTML +=
+      "<li>" + number + "</li>";
   } else {
     console.log("ATTENZIONE: non ha inserito un valore numerico.");
     break;
@@ -16,6 +16,8 @@ for (var i = 1; i <= 10; i++) {
 }
 
 console.log("Somma dei 10 numeri: " + sum);
+document.getElementById("sum-avg").innerHTML = "Somma dei 10 numeri: " + sum;
 
 var avg = (sum / 10).toFixed(2);
 console.log("Ecco la media: " + avg);
+document.getElementById("avg").innerHTML = "Media dei 10 numeri: " + avg;
