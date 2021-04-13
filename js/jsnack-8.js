6298fdc986dc0279841edaf76c6ba22362bf99e3
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-13 16:17:24 +0200

    collegamento js ad html

--- js/jsnack-8.js
+++ js/jsnack-8.js
@@ -2,6 +2,8 @@ var number_4 = prompt("Inserisci un numero con 4 cifre: ");
 var sum = 0;
 
 if (number_4.length == 4) {
+  document.getElementById("number-4").innerHTML =
+    "Numero inserito: " + number_4;
   parseInt(number_4);
   while (number_4) {
     console.log("Numero inserito: " + number_4);
@@ -9,9 +11,15 @@ if (number_4.length == 4) {
     number_4 = Math.floor(number_4 / 10);
     console.log("Somma numeri: ", sum);
   }
+  document.getElementById("sum-digits").innerHTML = "Somma delle cifre: " + sum;
 } else {
   var wrong_digits = number_4.length;
   console.log(
     `ATTENZIONE! Il numero inserito ha ${wrong_digits}, non 4 cifre!`
   );
+  document.getElementById("number-4").innerHTML =
+    "Numero inserito: " + number_4;
+  document.getElementById(
+    "sum-digits"
+  ).innerHTML = `ATTENZIONE! Il numero inserito ha ${wrong_digits}, non 4 cifre!`;
 }
