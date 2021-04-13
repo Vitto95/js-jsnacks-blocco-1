3b4ab02262888e402198ac7de9678a37fc1d5bb9
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-13 11:56:08 +0200

    aggiunta su html

--- js/jsnack-6.js
+++ js/jsnack-6.js
@@ -4,5 +4,15 @@ var somma_cubo = 0;
 for (var i = 1; i <= yourNumber; i++) {
   var currentNumber = i;
   somma_cubo += currentNumber ** 3;
+  document.getElementById("somma-cubo-parziale").innerHTML +=
+    "<li>" +
+    `Somma cubo parziale fino a ${currentNumber}: ` +
+    somma_cubo +
+    "</li>";
   console.log(somma_cubo);
 }
+
+document.getElementById("your-number-cubo").innerHTML =
+  "Il tuo numero: " + yourNumber;
+document.getElementById("somma-cubo").innerHTML =
+  `Somma dei cubi fino a ${yourNumber}: ` + somma_cubo;
